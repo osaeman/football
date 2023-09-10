a04e81f1453ea6de640b8814a4c1f7f9ada4ab22
osaeman <2015n2424@gmail.com>
2023-09-11 03:43:14 +0500

    dropdown menu

--- script/script.js
+++ script/script.js
@@ -6,3 +6,17 @@ const hamburger_menu = document.querySelector(".mobile-header-hamburger-menu");
 hamburger_button.addEventListener("click", () => {
   hamburger_menu.classList.toggle("mobile-header-hamburger-menu-2");
 });
+
+// Dropdown Nav Sub Menu
+const navSubMenuLink = document.querySelectorAll(".nav-sub-menu-link");
+const navSubMenuDropdown = document.querySelectorAll(".nav-sub-menu-dropdown");
+
+navSubMenuLink.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    navSubMenuDropdown[index].classList.add("nav-sub-menu-dropdown-show");
+  });
+
+  item.addEventListener("mouseout", () => {
+    navSubMenuDropdown[index].classList.remove("nav-sub-menu-dropdown-show");
+  });
+});
